467585b9ae56ff64550369a0f144ad8408905541
udituen <udituen@gmail.com>
2025-08-17 19:02:48 -0600

    fix document upload

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -66,8 +66,9 @@ with st.expander("💡 Try example questions"):
         st.markdown(f"- {q}")
 
 uploaded_file = st.file_uploader("Upload your file", type=["txt","pdf"])
-data = uploaded_file.read()
-st.info(data)
+if uploaded_file:
+    data = uploaded_file.read()
+    st.info(data)
 query = st.text_input("Ask a question:")
 
 if uploaded_file is not None:
